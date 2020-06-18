230a4df5055fbb4427997491ff1ff24adbc05a18
Tri Do <trido1903@gmail.com>
2020-06-18 10:22:53 -0700

    create spring initializer and added aws dependencies

---
+++ src/main/java/com/trido/springbootawsimageupload/SpringbootAwsImageUploadApplication.java
@@ -0,0 +1,13 @@
+package com.trido.springbootawsimageupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsImageUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trido/springbootawsimageupload/SpringbootAwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trido.springbootawsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
